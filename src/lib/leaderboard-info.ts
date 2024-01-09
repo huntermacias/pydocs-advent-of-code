b2658abdf97c6f11f321248f26a797caaafcdfbd
huntermacias <supercodersf@gmail.com>
2024-01-08 17:40:41 -0800

    bug: removed leaderboard data

--- src/lib/leaderboard-info.ts
+++ src/lib/leaderboard-info.ts
@@ -7,11 +7,11 @@ export const getLeaderboardInfo = async () => {
             id: true,
             username: true,
             imageUrl: true, // Including imageUrl if you want to display user's image
-            solvedProblems: {
-                select: {
-                    id: true // You only need to select 'id' or any single field for counting
-                }
-            }
+            // solvedProblems: {
+            //     select: {
+            //         id: true // You only need to select 'id' or any single field for counting
+            //     }
+            // }
         }
     });
 
@@ -20,13 +20,13 @@ export const getLeaderboardInfo = async () => {
         id: user.id,
         username: user.username,
         imageUrl: user.imageUrl,
-        solvedProblemsCount: user.solvedProblems.length
+        // complete: user.solvedProblems.length
     }))
-    .sort((a, b) => b.solvedProblemsCount - a.solvedProblemsCount) // Sort by solvedProblemsCount in descending order
-    .map((user, index) => ({
-        ...user,
-        rank: index + 1 // Assign rank based on sorted order
-    }));
+    // .sort((a, b) => b.solvedProblemsCount - a.solvedProblemsCount) // Sort by solvedProblemsCount in descending order
+    // .map((user, index) => ({
+    //     ...user,
+    //     rank: index + 1 // Assign rank based on sorted order
+    // }));
 
     return leaderboard;
 }
